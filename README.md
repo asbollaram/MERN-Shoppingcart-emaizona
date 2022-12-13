4a0540ae344994c40d71006c9c8219fe3a04059f
asbollaram <asbollaram@gmail.com>
2022-12-14 04:51:17 +0530

    implemented edit prodcuts an update

--- README.md
+++ README.md
@@ -177,9 +177,12 @@ Mern shopping cart created in Mongodb,expressJs, NodeJs,Bootstrap,React-bootstra
    3. fetch data
 37. Create Product
    1. create prodcuts button
-   2. imp;ement backend api
+   2. implement backend api
    3. handle on click
 38. Create Edit Product
     1. create edit button
     2. create edit product ui
     3. dispaly product info in the input boxes
+39. Implement Update Product
+   1. create edit product backend api
+   2. handle update click
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -33,7 +33,30 @@ productRouter.post(
     res.send({ message: 'Product Created', product });
   })
 );
-
+// edit product details
+productRouter.put(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (product) {
+      product.name = req.body.name;
+      product.slug = req.body.slug;
+      product.price = req.body.price;
+      product.image = req.body.image;
+      product.category = req.body.category;
+      product.brand = req.body.brand;
+      product.countInStock = req.body.countInStock;
+      product.description = req.body.description;
+      await product.save();
+      res.send({ message: 'Product Updated' });
+    } else {
+      res.status(404).send({ message: 'Product Not Found' });
+    }
+  })
+);
 //filter products
 const PAGE_SIZE = 3;
 //
